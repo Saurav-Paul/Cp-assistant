f377c85d16e0546e35cc4674371a2ee722c5716b
saurav <sauravpaul.sunny@gmail.com>
2021-05-01 21:55:24 +0600

    setup files added

--- system/__about__.py
+++ system/__about__.py
@@ -1,10 +1,10 @@
 # Python Version: 3.x
-__package_name__ = 'ai-virtual-assistant'
+__package_name__ = 'cp-assistant'
 __author__ = 'Saurav Paul'
 __email__ = 'sauravpaul.sunny@gmail.com'
 __license__ = 'MIT License'
-__url__ = 'https://github.com/Saurav-Paul/AI-virtual-assistant-python'
-__version_info__ = (1, 0, 4, 'alpha', 1)
+__url__ = 'https://github.com/Saurav-Paul/Cp-assistant'
+__version_info__ = (1, 0, 0, 'alpha', 1)
 __version__ = '.'.join(map(str, __version_info__[:3]))
 __description__ = 'CLI virtual assistant for competitive programming'
 
