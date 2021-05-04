4f42d98aa003f6cbfe426db44f26cc21c60fd04b
saurav <sauravpaul.sunny@gmail.com>
2021-05-04 22:40:58 +0600

    style(removed unused code):

--- settings/logs.py
+++ settings/logs.py
@@ -1,14 +1,6 @@
 import logging
 from settings.settings import DEBUG
 
-logging.basicConfig(
-    level=logging.ERROR,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[
-        logging.FileHandler(".log", 'w'),
-        logging.StreamHandler()
-    ]
-)
 logger = logging.getLogger()
 
 if DEBUG:
